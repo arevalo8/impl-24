91e7e056778d762c4656f9d55f58621b0806c9cc
arevalo8 <110304077+arevalo8@users.noreply.github.com>
2024-05-29 17:32:01 +0200

    add estructure, index.html, book.spec.ts and jest.config.js

---
+++ temas/abstraccion/typescript/book.spec.ts
@@ -0,0 +1,28 @@
+// book.spec.ts
+
+import { Book } from './book';
+
+describe('Book', () => {
+  let book: Book;
+
+  beforeEach(() => {
+    book = new Book('El nombre del viento', 'Patrick Rothfuss', 20.0);
+  });
+
+  it('tiene un título', () => {
+    expect(book.title).toBe('El nombre del viento');
+  });
+
+  it('tiene un autor', () => {
+    expect(book.author).toBe('Patrick Rothfuss');
+  });
+
+  it('tiene un precio', () => {
+    expect(book.price).toBe(20.0);
+  });
+
+  it('puede cambiar su precio', () => {
+    book.price = 25.0;
+    expect(book.price).toBe(25.0);
+  });
+});
