fbc9a9731bd7d1b81dc60f89f13cbebd86730e48
arevalo8 <110304077+arevalo8@users.noreply.github.com>
2024-05-29 17:50:17 +0200

    update tsconfig.json, jest.config.json and book.ts

--- temas/abstraccion/typescript/jest.config.js
+++ temas/abstraccion/typescript/jest.config.js
@@ -3,9 +3,9 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    roots: ['<rootDir>/src'],
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest',
+    roots: ['<rootDir>/tests'],
+    moduleNameMapper: {
+      '^src/(.*)$': '<rootDir>/src/$1',
     },
   };
   
--- temas/abstraccion/typescript/src/book.ts
+++ temas/abstraccion/typescript/src/book.ts
@@ -1,2 +1,12 @@
-const message: string = "Hello, World!"
-console.log(message)
+// src/book.ts
+
+export class Book {
+    constructor(
+      public title: string,
+      public author: string,
+      public price: number
+    ) {}
+  
+    // Métodos adicionales si es necesario
+  }
+  
