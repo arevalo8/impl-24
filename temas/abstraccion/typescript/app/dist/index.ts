c9818235c0434a0aa18a2f64f4132450a851c036
arevalo8 <javi8arevalo@gmail.com>
2024-06-22 18:38:23 +0200

    Update index.ts and package.json

--- temas/abstraccion/typescript/app/dist/index.ts
+++ temas/abstraccion/typescript/app/dist/index.ts
@@ -1,7 +1,6 @@
 // src/index.ts
 import { Book } from './book';
-import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser';
+
 
 // Crear una instancia de Book y usarla
 const myBook = new Book('El nombre del viento', 'Patrick Rothfuss', 20.0);
